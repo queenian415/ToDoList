9dc228008d368508e33ce917b2f6bb28f3e0840c
Tiffany Huang <queenian415@gmail.com>
2014-09-15 00:28:50 -0400

    IBAction added to Buttons. Navigation controller embeded. UI finished.

---
+++ ToDoList/AddToDoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddToDoViewController.swift
+//  ToDoList
+//
+//  Created by Tiffany Huang on 2014-09-15.
+//  Copyright (c) 2014 Tiffany Huang. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoList/ToDoItemTableViewController.h
@@ -0,0 +1,11 @@
+//
+//  ToDoItemTableViewController.h
+//  ToDoList
+//
+//  Created by Tiffany Huang on 2014-09-15.
+//  Copyright (c) 2014 Tiffany Huang. All rights reserved.
+//
+
+@interface ToDoItemTableViewController
+-(IBAction) unwindToList: (UIStoryboardSegue *)segue;
+@end
